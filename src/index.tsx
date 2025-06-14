f28cdbe35fc5d514b9c9897eef12906f0cdac7a3
Bala <balu.bintu08@gmail.com>
2025-06-14 10:19:29 +0530

    FIX: Clean and correct index.html for Vite build

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // This path is now correct
-import { ThemeProvider } from './components/ThemeSwitcher'; // This path is now correct
-import './index.css'; // This path is now correct
+import App from './App';
+import { ThemeProvider } from './components/ThemeSwitcher';
+
+// --- V V V THIS IS THE LINE THAT FIXES EVERYTHING V V V ---
+import './index.css';
+// --- ^ ^ ^ THIS IS THE LINE THAT FIXES EVERYTHING ^ ^ ^ ---
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <ThemeProvider> {/* <-- WRAPPER START */}
+      <ThemeProvider>
         <App />
-      </ThemeProvider> {/* <-- WRAPPER END */}
+      </ThemeProvider>
     </React.StrictMode>
   );
 }
