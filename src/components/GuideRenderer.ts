8b839cb8acf1928cfa0df2dc3a03f5f98cb5257c
Bala <balu.bintu08@gmail.com>
2025-07-04 00:13:37 +0530

    Deleted unused files and updated project structure

--- src/components/GuideRenderer.ts
+++
@@ -1,3 +0,0 @@
-// This file is no longer used as SVG template generation has been replaced by PDF generation.
-// The new PDF generation logic is in components/PdfGenerator.ts.
-// Keeping this file empty or deleting it. For this operation, content is removed.
--- types.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'bwip-js';
